3927738e4ef82b8860e3e2f2efa2a766fbf0dadf
ismaillcann <smlcan.551@gmail.com>
2023-10-07 19:26:39 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by İsmail CAN on 7.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
