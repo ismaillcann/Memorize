e092161b087ef19e87b266fad28a0a53ca17af11
ismaillcann <smlcan.551@gmail.com>
2023-10-12 18:08:34 +0300

    buttons added. buttons modified with images. lines of code edited in a regular form.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,45 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        let emojis = ["👻", "🎃", "🕷️", "😈"]  // [String] === Array<String>
+    let emojis = ["👻", "🎃", "🕷️", "😈", "♺", "♠️", "🎭"] // [String] === Array<String>
+    @State var cardCount: Int = 4
 
-        
+    var body: some View {
+        VStack {
+            cards
+            cardCountAdjusters
+        }
+
+        .padding()
+    }
+
+    var cards: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self){ index in
+            ForEach(0 ..< cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
         }
         .foregroundColor(.gray)
-        .padding()
+    }
+
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.title3)
+    }
+
+    var cardRemover: some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+
+        })
+    }
+
+    var cardAdder: some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+
+        })
     }
 }
 
 struct CardView: View {
     let content: String
     @State var isFaceUp = true
-    
+
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 20)
-            
+
             if isFaceUp == true {
-                
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                
+
                 Text(content).font(.largeTitle)
-            }
-            else {
+            } else {
                 base.fill()
             }
-                
         }
         .onTapGesture {
             isFaceUp.toggle()
         }
     }
 }
+
 #Preview {
     ContentView()
 }
